4873f0b745aa570190e7087757960bac7a887d19
Khaled Alam <khaledalam.net@gmail.com>
2019-08-10 21:05:37 +0200

    refactor and finish remain work.

--- main.cpp
+++ main.cpp
@@ -4,46 +4,49 @@
 // Description:
 // Arrow Shooting 2D Game using C++ and OpenGL
 
+#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
-
 #include <GL/glut.h>
 
 using namespace std;
 
-int cnt = 1;
-float gx2 = 20;
-float gy2 = 20;
-float theta = 0;
-int windowHeight = 500;
-int windowWidth = 500;
+const float gx2 = 20;
+const float gy2 = 20;
+const int windowHeight = 500;
+const int windowWidth = 500;
 const float screenLeft = 0.0f;
 const float screenRight = 500.0f;
 const float screenBottom = 0.0f;
 const float screenTop = 500.0f;
-const float minLimit = 70.0f;
 
-const float R = 3.5;
+const float rateChangeAngle = 0.025; // rate used to change angle when click (Up & Down)
+const float rateFireSpeed = 0.8;     // rate used for increasing radius wile firing
+const float rateHitDistance = 10.0;  // rate used for considering line hit dot
 
-const float PI = 3.14285714286;
+const int rateGameoverSeconds = 20; // rate used to set when game is over
 
-const int UP = -1;
-const int DOWN = 1;
+// const float PI = 3.14285714286;
 
-int recentAction = UP;
-bool run = true;
+bool firing = false;
+int score = 0;
 
-#define MaxNumPts 16
+time_t start, tillNow;
 
 // START Prototypes
+struct Point;
+struct Line;
 void myKeyboardFunc(unsigned char key, int x, int y);
 void myMouseFunc(int button, int state, int x, int y);
 void displayPoints(void);
 void addNewPoint(float x, float y);
 void initRendering();
 void resizeWindow(int w, int h);
-void fire();
 void goUp();
 void goDown();
+void generateDot();
+void writeText(string text, float x, float y);
+double distanceBetweenTwoPoints(Point A, Point B);
+
 // END Prototypes
 
 // START Structs
@@ -56,25 +59,125 @@ struct Point
         y = _y;
     }
     Point(){};
+
+    void draw()
+    {
+        glColor3f(rand() % 2, rand() % 2, rand() % 2);
+
+        glBegin(GL_POINTS);
+        glPointSize(10.0f); // wat
+        glVertex2f(x, y);
+        glEnd();
+    }
 };
 
 struct Line
 {
+    float angle;
+    float R_from; // radius from point(0, 0) to point(x_from, y_from)
+    float R_to;   // radius from point(0, 0) to point(x_to, y_to)
     Point from, to;
-    Line(Point _from, Point _to)
+    int lifeSteps = 1000;
+
+    Line(Point _from, Point _to, float _angle = 45.0, float _R_from = 0, float _R_to = 100.0)
     {
+        angle = _angle;
+        R_from = _R_from;
+        R_to = _R_to;
         from = _from;
         to = _to;
     }
     Line(){};
+
+    void draw()
+    {
+        glColor3f(1.0, 1.0, 0.0); //yellow
+        glBegin(GL_LINES);
+        glVertex2f(from.x, from.y);
+        glVertex2f(to.x, to.y);
+        glEnd();
+
+        /*
+            Coordinates of a point on a circle 
+
+            (0,0) ------------ (x,y)
+
+            x = R * cos(angle)
+            y = R * sin(angle)
+        */
+
+        from.x = (R_from * cos(angle));
+        from.y = (R_from * sin(angle));
+
+        to.x = (R_to * cos(angle));
+        to.y = (R_to * sin(angle));
+
+        // cout << angle << " (" << to.x << ", " << to.y << ")" << endl;
+    }
+
+    void fire()
+    {
+        R_to += rateFireSpeed;
+        R_from += rateFireSpeed;
+        this->changeAngle(angle);
+        this->draw();
+    }
+
+    void changeAngle(float newAngle)
+    {
+        if (firing)
+            return;
+
+        if (angle > 45.5)
+        {
+            angle = 45.5;
+            return;
+        }
+        if (angle < 44.02)
+        {
+            angle = 44.02;
+            return;
+        }
+
+        angle = newAngle;
+    }
+
+    bool insideBorder()
+    {
+        return to.x < screenRight && to.y < screenTop;
+    }
+
+    bool hitPoint(Point point)
+    {
+        return distanceBetweenTwoPoints(to, point) <= rateHitDistance;
+    }
+
+    void reset()
+    {
+        R_from = 0.0;
+        R_to = 100.0;
+        angle = 45.0;
+        this->changeAngle(angle);
+        this->draw();
+    }
 };
 // END Structs
 
+Point dot = Point(screenRight / 2, screenTop / 2);
+
 Line arrow = Line(
-    Point(screenLeft, screenTop / 2),
-    Point(screenRight / 5, screenTop / 2));
+    Point(0, 0),
+    Point(100 * cos(45.0), 100 * sin(45.0)),
+    45.0,
+    0.0,
+    100.0);
 
 const Line defaultArrow = arrow;
+
+double distanceBetweenTwoPoints(Point A, Point B)
+{
+    return sqrt(pow(A.x - B.x, 2) + pow(A.y - B.y, 2));
+}
 
 // START Events
 void onKeyboardEvent(unsigned char key, int x, int y)
@@ -86,7 +189,10 @@ void onKeyboardEvent(unsigned char key, int x, int y)
         break;
     case 32:
     case 13:
-        fire();
+        firing = true;
+        break;
+    case 'r': // r key
+        time(&start);
         break;
     }
 }
@@ -101,159 +207,94 @@ void SpecialInput(int key, int x, int y)
     case GLUT_KEY_UP:
         goUp();
         break;
-    case GLUT_KEY_RIGHT:
     case GLUT_KEY_LEFT:
-        arrow = defaultArrow;
+        arrow.reset();
+        break;
+    case GLUT_KEY_RIGHT:
+        firing = true;
         break;
     }
 }
 
-void onMouseEvent(int button, int state, int x, int y)
-{
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-    {
-        //scale correction:
-        float xPos = ((float)x) / ((float)(windowWidth * (1 / screenRight)));
-        float yPos = ((float)y) / ((float)(windowHeight * (1 / screenTop)));
-        yPos = screenTop - yPos; // Flip  (y is from top)
-        cout << xPos << " " << yPos << endl;
-        glutPostRedisplay();
-    }
-}
-
-void onIdleEvent()
-{
-    cout << "test" << endl;
-    // delay(700);
-}
-
-void onPassiveMotionEvent(int x, int y)
-{
-    cout << x << " " << y << endl;
-    glutPostRedisplay();
-    // delay(200);
-}
-
-void onMotionEvent(int x, int y)
-{
-    glutPostRedisplay();
-    // delay(200);
-}
-
 // END Events
 
-void delay(int seconds)
+void writeText(string text, float x, float y)
 {
-    int miSeconds = 1000 * seconds;
-    clock_t startTime = clock();
-    while (clock() < startTime + miSeconds)
-        ;
-}
-
-// void writeText(string text, float x, float y)
-// {
-//     glColor3f(1.0, 1.0, 0.0); //yellow
-//     glRasterPos2i(x, y);
-//     int len = text.length();
-//     for (int i = 0; i < len; i++)
-//     {
-//         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
-//         glutPostRedisplay();
-//     }
-
-//     glFlush();
-// }
-
-void drawArrow(Line x)
-{
-    glColor4f(1, 1, 1, 1);
-    glBegin(GL_LINES);
-    glVertex2f(x.from.x, x.from.y);
-    glVertex2f(x.to.x, x.to.y);
-    glEnd();
-
-    glBegin(GL_LINES);
-    glVertex2f((screenRight / 5), screenTop / 2); // (1/5) ---
-    glVertex2f((screenRight / 5) - 25, screenTop / 2 - 15);
-    glEnd();
-
-    glBegin(GL_LINES);
-    glVertex2f((screenRight / 5), screenTop / 2); // (1/5) ---
-    glVertex2f((screenRight / 5) - 25, screenTop / 2 + 15);
-    glEnd();
-
-    if (run)
+    glColor3f(1.0, 1.0, 0.0); //yellow
+    glRasterPos2i(x, y);
+    int len = text.length();
+    for (int i = 0; i < len; i++)
     {
-        fire();
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
     }
-}
+    glutPostRedisplay();
 
-void displyInitArrow(float angle = 90.0)
-{
-}
-
-void fire()
-{
-    cout << "fired" << endl;
-
-    cout << arrow.to.x << endl;
-    if (arrow.to.x >= 500)
-    {
-        run = false;
-        arrow.to.x = 499;
-        return;
-    }
-    arrow.from.x += 0.1, arrow.to.x += 0.1;
+    glFlush();
 }
 
 void goUp()
 {
-    if (arrow.to.x <= minLimit && recentAction == UP)
-    {
-        arrow.to.x = minLimit;
-        return;
-    }
-
-    // theta += 0.05;            //get the current angle
-    // float x = R * sin(theta); //calculate the x component
-    // float y = R * cos(theta); //calculate the y component
-
-    cout << "up! " << arrow.to.x << " " << arrow.to.y << endl;
-    arrow.to.x -= sin(0.7);
-    arrow.to.y += cos(0.7);
+    arrow.changeAngle(arrow.angle + rateChangeAngle);
 }
 
 void goDown()
 {
-    if (arrow.to.x <= minLimit && recentAction == DOWN)
-    {
-        arrow.to.x = minLimit;
-        return;
-    }
+    arrow.changeAngle(arrow.angle - rateChangeAngle);
+}
 
-    cout << "down! " << arrow.to.x << " " << arrow.to.y << endl;
-
-    // if (arrow.to.x >= 100)
-    // {
-    //     cout << "Y" << endl;
-    //     arrow.to.x -= sin(0.7);
-    //     arrow.to.y += cos(0.7);
-    // }
-    // else
-    // {
-    //     arrow.to.x += sin(0.7);
-    //     arrow.to.y -= cos(0.7);
-    // }
+void generateDot()
+{
+    dot = Point((rand() % (int)screenRight), (rand() % (int)screenTop));
 }
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor4f(1, 1, 1, 1);
+    glColor3f(1.0, 1.0, 0.0);
 
-    drawArrow(arrow);
+    time(&tillNow);
+    int dif = difftime(tillNow, start);
 
-    glutPostRedisplay();
+    bool gameover = (dif >= rateGameoverSeconds);
+    if (gameover)
+    {
+        firing = false;
+        arrow.reset();
+        writeText("Score: " + to_string(score) + "  in " + to_string(rateGameoverSeconds) +
+                      " second" + (rateGameoverSeconds > 1 ? "s" : "") + "!  press [r] key",
+                  screenRight / 6, screenTop / 2);
+    }
+    else
+    {
+        arrow.draw();
+        dot.draw();
+
+        writeText(to_string(dif) + " second" + (dif > 1 ? "s" : "") + "   |   score: " + to_string(score),
+                  screenRight / 4, screenTop - 20);
+    }
+
+    if (firing)
+    {
+        if (arrow.insideBorder())
+        {
+            arrow.fire();
+            double x = distanceBetweenTwoPoints(arrow.to, dot);
+
+            if (arrow.hitPoint(dot))
+            {
+                score += (50 / distanceBetweenTwoPoints(arrow.to, dot));
+                generateDot();
+            }
+        }
+        else
+        {
+            firing = false;
+            arrow.reset();
+        }
+    }
+
+    if (!gameover)
+        glutPostRedisplay();
 
     glFlush();
 }
@@ -283,6 +324,7 @@ void resizeWindow(int w, int h)
 
 int main(int argc, char **argv)
 {
+    time(&start);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(windowWidth, windowHeight);
@@ -295,10 +337,6 @@ int main(int argc, char **argv)
     glutReshapeFunc(resizeWindow);
     glutKeyboardFunc(onKeyboardEvent);
     glutSpecialFunc(SpecialInput);
-    glutMouseFunc(onMouseEvent);
-    // glutPassiveMotionFunc(onPassiveMotionEvent);
-    // glutMotionFunc(onMotionEvent);
-    // glutIdleFunc(onIdleEvent);
 
     glutMainLoop();
 
